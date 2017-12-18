a1c50e631b31787ef24188fa4491a915efc7e118
napalm255 <napalm255@gmail.com>
2017-12-17 20:33:03 -0500

    added sns

--- index.py
+++ index.py
@@ -1,12 +1,30 @@
+"""Commute Traffic to SNS."""
+
+import logging
 import json
-import datetime
+import boto3
+
+SNS = boto3.client('sns')
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
+    """Lambda handler."""
+    # pylint: disable=unused-argument
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    logger.info(event)
+
+    # vars
+    topic_arn = event['topic_arn']
+    message = {"foo": "bar"}
+
+    logger.info(message)
+    response = SNS.publish(
+        TopicArn=topic_arn,
+        Message=json.dumps({'default': json.dumps(message)}),
+        MessageStructure='json'
+    )
     return {'statusCode': 200,
-            'body': json.dumps(data),
+            'body': json.dumps(response),
             'headers': {'Content-Type': 'application/json'}}
