50584c8a3dce77108236610a4a2f1481b9f03037
napalm255 <napalm255@gmail.com>
2017-12-18 16:14:03 -0500

    changed timestamp

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """Commute Traffic to SNS."""
 
-from time import time
+from datetime import datetime
 import logging
 import os
 import json
@@ -23,7 +23,7 @@ def get_commute_duration(google_api_key, origin, destination, **kwargs):
     query = requests.get(url + url_data)
     results = query.json()
     details = results['rows'][0]['elements'][0]
-    data = {'timestamp': str(time()),
+    data = {'timestamp': str(datetime.utcnow().isoformat()),
             'origin': str(origin),
             'destination': str(destination)}
     data.update(details)
